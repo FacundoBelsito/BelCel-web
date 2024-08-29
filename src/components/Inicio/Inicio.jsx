70a0792385a16a47a3a0d0c7c9d954624f9ba7f3
Facundo <=>
2024-08-29 19:51:56 -0300

    arreglo de logo

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -8,7 +8,7 @@ const Inicio = () => {
   return (
     <main className="w-full h-[100vh] flex justify-center items-center gap-x-10">
       <div className='text-flicker-out-glow div-logo'>
-        <img className=" desktop:w-[28rem]  laptop:w-80 logo-del-orto" src="https://i.postimg.cc/xd429bVv/7.png" alt="" />
+        <img className=" desktop:w-[28rem]  laptop:w-80 logo-del-orto" src="https://raw.githubusercontent.com/Lucascelse1/images/main/18.png" alt="" />
       </div>
       <div className='text-flicker-out-glow texto-del-orto'>
         <p className="w-74 text-center desktop:mt-16 custom-md:mt-96 desktop:text-4xl laptop:text-4xl text-3xl tracking-widest">TRANSFORMAMOS<br /> TUS IDEAS <br /> EN DESARROLLOS WEB</p>
